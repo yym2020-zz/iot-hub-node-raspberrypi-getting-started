8356e94594aa7653c0e451bc91fe17554d473bd8
ZhijunZhao <zhijun.zhao@outlook.com>
2016-10-12 09:59:57 +0800

    use storage connection string from config file instead of retrieving … (#7)
    * use storage connection string from config file instead of retrieving it via az command
    
    * remove message count info from azure table console output

--- Lesson3/azure-table.js
+++ Lesson3/azure-table.js
@@ -3,75 +3,51 @@
 */
 'use strict';
 
-var exec = require('child_process').exec;
 var moment = require('moment');
 var storage = require('azure-storage');
 var config = require('./config.json');
-var params = require('./arm-template-param.json').parameters;
 
 var stopReadAzureTable = false;
 
 var readAzureTable = function () {
-  var command = 'az storage account show-connection-string -g ' + config.resource_group + ' -n ' + params.resoucePrefix.value + 'storage';
-  exec(command, function (err, stdout, stderr) {
-    if (err) {
-      console.error('[Azure Table] ERROR:\n' + err);
-      return;
-    }
-    if (stderr) {
-      console.error('[Azure Table] Message from STDERR:\n' + stderr);
-    }
-    if (stdout) {
-      var connStr = JSON.parse(stdout).connectionString;
-      if (connStr) {
-        var tableService = storage.createTableService(connStr);
-        var condition = 'PartitionKey eq ? and RowKey gt ? ';
-        var tableName = 'DeviceData';
-        var timestamp = moment.utc().format('hhmmssSSS');
+  var tableService = storage.createTableService(config.azure_storage_connection_string);
+  var condition = 'PartitionKey eq ? and RowKey gt ? ';
+  var tableName = 'DeviceData';
+  var timestamp = moment.utc().format('hhmmssSSS');
 
-        var messageCount = 0;
+  var readNewMessages = function () {
+    var query = new storage.TableQuery().where(condition, moment.utc().format('YYYYMMDD'), timestamp);
 
-        var readNewMessages = function () {
-          var query = new storage.TableQuery().where(condition, moment.utc().format('YYYYMMDD'), timestamp);
-
-          tableService.queryEntities(tableName, query, null, function (error, result) {
-            if (error) {
-              // Table not found.
-              if (error.statusCode && error.statusCode == 404) {
-                console.error(
-                  '[Azure Table] ERROR: Table not found. Something might be wrong. Please go to troubleshooting page for more information.')
-              } else {
-                console.error('[Azure Table] ERROR:\n' + error);
-              }
-              setTimeout(readNewMessages, 0);
-              return;
-            }
-
-            // result.entries contains entities matching the query
-            if (result.entries.length > 0) {
-              for (var i = 0; i < result.entries.length; i++) {
-                ++messageCount;
-                console.log('[Azure Table] Read message #' + messageCount + ': ' + result.entries[i].message['_'] + '\n');
-
-                if (result.entries[i].RowKey['_'] > timestamp) {
-                  timestamp = result.entries[i].RowKey['_'];
-                }
-              }
-            }
-            if (!stopReadAzureTable) {
-              setTimeout(readNewMessages, 0);
-            }
-          });
+    tableService.queryEntities(tableName, query, null, function (error, result) {
+      if (error) {
+        // Table not found.
+        if (error.statusCode && error.statusCode == 404) {
+          console.error(
+            '[Azure Table] ERROR: Table not found. Something might be wrong. Please go to troubleshooting page for more information.')
+        } else {
+          console.error('[Azure Table] ERROR:\n' + error);
         }
-
-        readNewMessages();
-      } else {
-        console.error('[Azure Table] ERROR: Fail to get connection string of Azure Storage account.')
+        setTimeout(readNewMessages, 0);
+        return;
       }
-    } else {
-      console.error('[Azure Table] ERROR: No output when getting connection string of Azure Storage account.');
-    }
-  });
+
+      // result.entries contains entities matching the query
+      if (result.entries.length > 0) {
+        for (var i = 0; i < result.entries.length; i++) {
+          console.log('[Azure Table] Read message: ' + result.entries[i].message['_'] + '\n');
+
+          if (result.entries[i].RowKey['_'] > timestamp) {
+            timestamp = result.entries[i].RowKey['_'];
+          }
+        }
+      }
+      if (!stopReadAzureTable) {
+        setTimeout(readNewMessages, 0);
+      }
+    });
+  }
+
+  readNewMessages();
 }
 
 var cleanup = function () {
