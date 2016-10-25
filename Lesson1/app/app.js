cfef96eb8e81a9e43a5e77fefbb35ad79ff62bc1
Kevin Zhao <kvn.zhl@gmail.com>
2016-10-25 16:16:04 +0800

    [Lesson1] blink LED for 20 times instead of forever (#31)

--- Lesson1/app/app.js
+++ Lesson1/app/app.js
@@ -9,6 +9,8 @@ var wpi = require('wiring-pi');
 var CONFIG_PIN = 7;
 // Blink interval in ms
 var INTERVAL = 2000;
+// Total blink times
+var MAX_BLINK_TIMES = 20;
 
 wpi.setup('wpi');
 wpi.pinMode(CONFIG_PIN, wpi.OUTPUT);
@@ -18,6 +20,11 @@ var blinkTimes = 0;
  * Blink LED and log information to console.
  */
 function blinkLED() {
+  // Terminate process after blinking LED for max allowed times
+  if (blinkTimes >= MAX_BLINK_TIMES) {
+    process.exit(0);
+  }
+
   blinkTimes++;
   console.log("[Device] #" + blinkTimes + " Blink LED \n");
 
